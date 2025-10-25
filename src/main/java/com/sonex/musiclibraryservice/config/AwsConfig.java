af9f960b6f1c31bc457c69f8cf205202885161b2
Dinushka-Aththanayake <161476349+Dinushka-Aththanayake@users.noreply.github.com>
2025-10-25 21:50:17 +0530

    update aws config

--- src/main/java/com/sonex/musiclibraryservice/config/AwsConfig.java
+++ src/main/java/com/sonex/musiclibraryservice/config/AwsConfig.java
@@ -1,19 +1,30 @@
 package com.sonex.musiclibraryservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AwsConfig {
 
+    @Value("${AWS_ACCESS_KEY_ID:}")
+    private String accessKeyId;
+
+    @Value("${AWS_SECRET_ACCESS_KEY:}")
+    private String secretKey;
+
     @Bean
     public S3Client s3Client() {
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKeyId, secretKey);
+
         return S3Client.builder()
-                .region(Region.AP_SOUTHEAST_1) // Change to your region
-                .credentialsProvider(ProfileCredentialsProvider.create())
+                .region(Region.AP_SOUTHEAST_1)
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
                 .build();
     }
 }
--- src/main/java/com/sonex/musiclibraryservice/config/KafkaConsumerConfig.java
+++ src/main/java/com/sonex/musiclibraryservice/config/KafkaConsumerConfig.java
@@ -2,8 +2,13 @@ package com.sonex.musiclibraryservice.config;
 
 
 import com.sonex.musiclibraryservice.model.primary.MoodListnerEvent;
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import com.sonex.musiclibraryservice.model.primary.GenreListnerEvent;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -20,11 +25,42 @@ import java.util.HashMap;
 @Configuration
 @EnableKafka
 public class KafkaConsumerConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumerConfig.class);
 
+    private Map<String, Object> getCommonKafkaConfig() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "boot-ctavlxrz.c2.kafka-serverless.us-east-1.amazonaws.com:9098");
+        configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        // Increase timeouts for MSK Serverless
+        configProps.put(ConsumerConfig.REQUEST_TIMEOUT_MS_CONFIG, "120000");
+        configProps.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "60000");
+        configProps.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, "5000");
+        configProps.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, "600000");
+        configProps.put(ConsumerConfig.CONNECTIONS_MAX_IDLE_MS_CONFIG, "540000");
+
+        // Enable detailed logging
+        configProps.put(ConsumerConfig.METADATA_MAX_AGE_CONFIG, "300000");
+
+        // MSK IAM Authentication
+        configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+        configProps.put(SaslConfigs.SASL_MECHANISM, "AWS_MSK_IAM");
+        configProps.put(SaslConfigs.SASL_JAAS_CONFIG, "software.amazon.msk.auth.iam.IAMLoginModule required;");
+        configProps.put(SaslConfigs.SASL_CLIENT_CALLBACK_HANDLER_CLASS, "software.amazon.msk.auth.iam.IAMClientCallbackHandler");
+
+        // AWS Region
+        configProps.put("aws.region", "us-east-1");
+
+        LOGGER.info("Kafka config initialized with bootstrap servers: {}",
+                configProps.get(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG));
+
+        return configProps;
+    }
     @Bean("genreConsumerFactory")
     public ConsumerFactory<String, GenreListnerEvent> genreConsumerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "boot-ctavlxrz.c2.kafka-serverless.us-east-1.amazonaws.com:9098");
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "music-genre-classifier");
         configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
@@ -53,6 +89,13 @@ public class KafkaConsumerConfig {
     public ConcurrentKafkaListenerContainerFactory<String, GenreListnerEvent> genreKafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, GenreListnerEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(genreConsumerFactory());
+        // Set acknowledgment mode
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+
+        // Add error handler
+        factory.setCommonErrorHandler(new org.springframework.kafka.listener.DefaultErrorHandler());
+
+        LOGGER.info("Genre Kafka listener container factory initialized");
         return factory;
     }
 
@@ -60,6 +103,13 @@ public class KafkaConsumerConfig {
     public ConcurrentKafkaListenerContainerFactory<String, MoodListnerEvent> moodKafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, MoodListnerEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(moodConsumerFactory());
+        // Set acknowledgment mode
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+
+        // Add error handler
+        factory.setCommonErrorHandler(new org.springframework.kafka.listener.DefaultErrorHandler());
+
+        LOGGER.info("Mood Kafka listener container factory initialized");
         return factory;
     }
 }

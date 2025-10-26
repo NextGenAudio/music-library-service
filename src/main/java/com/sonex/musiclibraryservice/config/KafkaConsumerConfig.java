99efd0e66b13fc222fa939e690b1165e96238d89
Dinushka-Aththanayake <161476349+Dinushka-Aththanayake@users.noreply.github.com>
2025-10-26 17:46:18 +0530

    modify kafka url

--- src/main/java/com/sonex/musiclibraryservice/config/KafkaConsumerConfig.java
+++ src/main/java/com/sonex/musiclibraryservice/config/KafkaConsumerConfig.java
@@ -74,7 +74,7 @@ public class KafkaConsumerConfig {
     @Bean("moodConsumerFactory")
     public ConsumerFactory<String, MoodListnerEvent> moodConsumerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "boot-ctavlxrz.c2.kafka-serverless.us-east-1.amazonaws.com:9098");
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "music-mood-classifier");
         configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
